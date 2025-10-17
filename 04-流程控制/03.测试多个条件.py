64146e9293c0e6222c5707e62ef5090dbafa48d6
GaoXiong <xiong.gao@shopee.com>
2025-10-17 19:19:37 +0800

    chore: dev

---
+++ 04-流程控制/03.测试多个条件.py
@@ -0,0 +1,11 @@
+
+requested_toppings = ['mushrooms','extra cheese']
+
+# 类似 switch 语句
+if 'mushrooms'in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni'in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese'in requested_toppings:
+    print("Adding extra cheese.")
+print("\nFinished making your pizza!")
