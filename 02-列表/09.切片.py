6e4273964308fc2e8af5435a868bff911746ff93
GaoXiong <xiong.gao@shopee.com>
2025-10-17 18:49:40 +0800

    chore: dev

---
+++ 02-列表/09.切片.py
@@ -0,0 +1,13 @@
+# 提取前三个元素
+players = ['charles','martina','michael','florence','eli']
+print(players[0:3])
+
+# 提取最后三个元素
+players = ['charles','martina','michael','florence','eli']
+print(players[-3:])
+
+
+players = ['charles','martina','michael','florence','eli']
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
