076adf956a525f34c4b4bab1baac6f3f650eba87
GaoXiong <xiong.gao@shopee.com>
2025-10-19 22:20:59 +0800

    chore: dev

--- 01-字符串/01.分割.py
+++ 01-字符串/01.分割.py
@@ -2,3 +2,16 @@ str = "hello world"
 print(str.split())
 print(str.split(' '))
 print(str.split('o'))
+
+def split_string(str, delimiter):
+    return str.split(delimiter)
+
+print(split_string("hello world", " "))
+print(split_string("hello world", "o"))
+print(split_string("hello world", "l"))
+print(split_string("hello world", "h"))
+print(split_string("hello world", "w"))
+print(split_string("hello world", "r"))
+print(split_string("hello world", "d"))
+print(split_string("hello world", "e"))
+print(split_string("hello world", "f"))
