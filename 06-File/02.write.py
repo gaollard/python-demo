fa02d2ca9ac7d91a02127c2cd8cbdfd800dee25e
GaoXiong <xiong.gao@shopee.com>
2025-10-19 22:12:11 +0800

    chore: dev

---
+++ 06-File/02.write.py
@@ -0,0 +1,9 @@
+filename = 'programming.txt'
+
+with open(filename, 'w') as file_object:
+    file_object.write("I love programming.")
+
+filename = 'programming.txt'
+with open(filename,'w') as file_object:
+    file_object.write("I love programming.")
+    file_object.write("I love creating new games.")
