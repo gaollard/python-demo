ca12c5e550ab8b5d339e7579ed8ae89fa8587df5
GaoXiong <xiong.gao@shopee.com>
2025-10-20 10:06:46 +0800

    chore: dev

--- 09-venv/polls/urls.py
+++ 09-venv/polls/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('hello', views.hello_world),
     path('query_params', views.query_params),
+    path('submit_form', views.submit_form),
+    path('get_only', views.get_only_view),
+    path('post_only', views.post_only_view),
+    path('get_post', views.get_post_view),
 ]
--- 09-venv/polls/views.py
+++ 09-venv/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods, require_GET, require_POST
 
 # Create your views here.
 def hello_world(request):
@@ -20,3 +21,32 @@ def hello_world(request):
 
 def query_params(request):
     return HttpResponse("query_params")
+
+def submit_form(request):
+    if request.method == 'GET':
+        # 处理GET请求 - 显示表单
+        return HttpResponse("显示表单页面")
+    elif request.method == 'POST':
+        # 处理POST请求 - 处理表单提交
+        return HttpResponse("处理表单提交")
+    else:
+        # 其他HTTP方法
+        return HttpResponse("不支持的请求方法")
+
+# 使用装饰器限制只允许GET请求
+@require_GET
+def get_only_view(request):
+    return HttpResponse("这个视图只接受GET请求")
+
+# 使用装饰器限制只允许POST请求
+@require_POST
+def post_only_view(request):
+    return HttpResponse("这个视图只接受POST请求")
+
+# 使用装饰器限制允许GET和POST请求
+@require_http_methods(["GET", "POST"])
+def get_post_view(request):
+    if request.method == 'GET':
+        return HttpResponse("GET请求处理")
+    else:  # POST
+        return HttpResponse("POST请求处理")
