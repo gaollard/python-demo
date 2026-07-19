0fa0b0b36167828116fb263d7f30ae9ff12655cc
GaoXiong <xiong.gao@shopee.com>
2026-07-19 22:01:11 +0800

    chore(*): docs

--- 02-basic/03-列表/13.列表推导式.py
+++ 02-basic/03-列表/13.列表推导式.py
@@ -5,3 +5,5 @@ print(squares) # [1, 4, 9, 16, 25]
 
 squares = [x ** 2 for x in numbers if x % 2 == 0]
 print(squares) # [4, 16]
+
+print([x ** 2 for x in range(1, 10)]) # [1, 4, 9, 16, 25, 36, 49, 64, 81]
