bef5b1123a3b52a75d631fd5234569914df739f1
GaoXiong <xiong.gao@shopee.com>
2026-07-19 12:53:26 +0800

    chore(*): docs

---
+++ 02-basic/12-运算符/01.py
@@ -0,0 +1,35 @@
+# 算术运算符
+a = 10
+b = 20
+c = a + b
+print(c)
+
+# 赋值运算符
+a = 10
+b = 20
+c = a + b
+print(c)
+
+# 比较运算符
+a = 10
+b = 20
+c = a > b
+print(c)
+
+# 逻辑运算符
+a = True
+b = False
+c = a and b
+print(c)
+
+# 成员运算符
+a = [1, 2, 3, 4, 5]
+b = 3
+c = b in a
+print(c)
+
+# 身份运算符
+a = [1, 2, 3, 4, 5]
+b = [1, 2, 3, 4, 5]
+c = a is b
+print(c)
