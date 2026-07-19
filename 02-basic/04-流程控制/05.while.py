d0f71881000c7a6518ee65fc4eeed39f3527e21a
GaoXiong <xiong.gao@shopee.com>
2026-07-19 21:24:26 +0800

    chore(*): docs

---
+++ 02-basic/04-流程控制/05.while.py
@@ -0,0 +1,18 @@
+# while 基本用法
+age = 0
+num = 0
+
+while age < 18:
+    age += 1
+
+print("你已经成年了 " + str(age))
+
+# while else
+
+
+num = 0
+while num < 10:
+  num += 1
+  print(num)
+else:
+  print("结束")
---
+++ 02-basic/04-流程控制/06.for.py
@@ -0,0 +1,21 @@
+num = 1
+# for range 基本用法
+for i in range(1, 10):
+  print(i)
+
+
+# for range 步长
+for i in range(1, 10, 2):
+  print(i)
+
+# for range 0-9
+for i in range(10):
+  print(i)
+
+
+strings = "hello"
+
+for i in strings:
+  print(i)
+else:
+  print("结束")
