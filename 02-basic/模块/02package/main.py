616d071a8fb8755cd23c21893328a0716da6a243
GaoXiong <xiong.gao@shopee.com>
2026-07-23 22:45:46 +0800

    chore(*): docs

--- 02-basic/模块/02package/main.py
+++ 02-basic/模块/02package/main.py
@@ -1,13 +1,14 @@
 import utils.circle_func as circle
 import utils.my_fun as my_fun
 from utils.circle_func import circle_area
-from core.db.connect import db_connect
 
 print(circle.circle_area(10))
 my_fun.say_hello()
 my_fun.say_goodbye()
 
 print(circle_area(10))
+
+from core.db.connect import db_connect
 print(db_connect())
 
 # logger 目录不是包，不能直接导入
